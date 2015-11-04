f2c78e68916405ee68972bf761da8d480188718f
Nathan McGuire <trusty007@nathans-mbp.gateway>
2015-11-05 10:29:17 +1100

    Initial Commit

---
+++ rpg_oop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rpg_oop
+//
+//  Created by Nathan McGuire on 5/11/2015.
+//  Copyright © 2015 Off Da Husk Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
